5f24109e6917fa7a5ab7a1ad1fc6188eb849b1a9
meshachvijaya <meshachvijaya@gmail.com>
2024-01-19 16:57:23 +0700

    create banner and slider with use effect

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,7 +1,60 @@
-import React from "react";
+"use client";
+
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+
+const data = [
+  {
+    id: 1,
+    title: "The best latte for every late game",
+    image: "/latte.jpg",
+  },
+  {
+    id: 2,
+    title: "Express yourself with favourite espresso",
+    image: "/espresso.jpg",
+  },
+  {
+    id: 3,
+    title: "Mocachino for every ratio",
+    image: "/mocha.jpg",
+  },
+];
 
 const Slider = () => {
-  return <div>Slider</div>;
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(
+      () =>
+        setCurrentSlide((prev) => (prev === data.length - 1 ? 0 : prev + 1)),
+      2000
+    );
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]">
+      {/* Text Container */}
+      <div className="h-1/2 flex items-center justify-center flex-col gap-8 text-green-900 font-bold ">
+        <h1 className="text-5xl text-center uppercase p-4 md:text-6xl xl:text-7xl">
+          {data[currentSlide].title}
+        </h1>
+        <button className="bg-green-900 text-white px-8 py-2 rounded-full font-semibold">
+          Pesan Sekarang
+        </button>
+      </div>
+      {/* Image Container */}
+      <div className="w-full h-1/2 relative">
+        <Image
+          src={data[currentSlide].image}
+          alt=""
+          fill
+          className="object-cover"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Slider;
